4c99446bdc15e2199ca5df1943871823ba06ec2a
Dimasnoufal <dimasnoufal26@gmail.com>
2023-05-11 23:38:22 +0700

    Initial commit

---
+++ app/src/main/java/com/dimasnoufal/latihankotlinandroid/ui/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.dimasnoufal.latihankotlinandroid.ui
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import com.dimasnoufal.latihankotlinandroid.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupSplash()
+    }
+
+    private fun setupSplash() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val controller = window.insetsController
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
